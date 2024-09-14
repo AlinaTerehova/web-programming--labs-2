fa5e65be9d13ced476b9b7af994ed3a56727c787
Алина Терехова <terehovalinas@yandex.ru>
2024-09-14 19:42:05 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!document>
+<html>
+    <head>
+        <title> Терехова Алина Сергеевна, Лабораторная 1</title>
+    <head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer> 
+            &copy; Алина Терехова, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+    """
