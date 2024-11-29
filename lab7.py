ee2743e92facb1b47955bd49df6f5cc4bd9114e2
Алина Терехова <terehovalinas@yandex.ru>
2024-11-29 11:55:08 +0700

    jsonyfy

--- lab7.py
+++ lab7.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, render_template, abort, request
-
+from flask import Blueprint, render_template, abort, request, jsonify
 
 lab7 = Blueprint('lab7', __name__)
 
@@ -55,13 +54,13 @@ films = [
 
 @lab7.route('/lab7/rest-api/films/', methods=['GET'])
 def get_films():
-    return films
+    return jsonify(films)
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
 def get_film(id):
     if id < 0 or id >= len(films):
         abort(404)
-    return films[id] 
+    return jsonify(films[id])
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE'])
 def del_film(id):
@@ -75,10 +74,10 @@ def put_film(id):
     if id < 0 or id >= len(films):
         abort(404)
     film = request.get_json()
-    if film ['description'] == '':
-        return {'description': 'Заполните описание'}, 400
+    if film['description'] == '':
+        return jsonify({'description': 'Заполните описание'}), 400
     films[id] = film
-    return films[id]
+    return jsonify(films[id])
 
 @lab7.route('/lab7/rest-api/films/', methods=['POST'])
 def add_film():
@@ -86,6 +85,6 @@ def add_film():
     if not film:
         abort(400)
     if film.get('description', '') == '':
-        return {'description': 'Заполните описание'}, 400
+        return jsonify({'description': 'Заполните описание'}), 400
     films.append(film)
-    return film, 201
+    return jsonify(film), 201
