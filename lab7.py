9019e6785aaae7df18e3233249f73c1b0b131db5
Алина Терехова <terehovalinas@yandex.ru>
2024-11-29 12:24:03 +0700

    самостоятельные задания

--- lab7.py
+++ lab7.py
@@ -76,6 +76,8 @@ def put_film(id):
     film = request.get_json()
     if film['description'] == '':
         return jsonify({'description': 'Заполните описание'}), 400
+    if not film.get('title'):
+        film['title'] = film['title_ru']
     films[id] = film
     return jsonify(films[id])
 
@@ -86,5 +88,7 @@ def add_film():
         abort(400)
     if film.get('description', '') == '':
         return jsonify({'description': 'Заполните описание'}), 400
+    if not film.get('title'):
+        film['title'] = film['title_ru']
     films.append(film)
     return jsonify(film), 201
