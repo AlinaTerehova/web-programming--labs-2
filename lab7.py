1b7f062521828ef6dfa8f1551c77237ceacb6194
Алина Терехова <terehovalinas@yandex.ru>
2024-12-01 13:08:33 +0700

    создание бд

--- lab7.py
+++ lab7.py
@@ -1,126 +1,132 @@
-from flask import Blueprint, render_template, abort, request, jsonify
-from datetime import datetime
+from flask import Blueprint, render_template, request, jsonify, abort, current_app
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 lab7 = Blueprint('lab7', __name__)
+
+def db_connect():
+    conn = psycopg2.connect(
+        host='127.0.0.1',
+        database='alina_terehova_knowledge_base',
+        user='alina_terehova_knowledge_base',
+        password='12345'
+    )
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    return conn, cur
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
 
 @lab7.route('/lab7/')
 def lab():
     return render_template('lab7/lab7.html')
 
-films = [
-    {
-        "title": "The Intern",
-        "title_ru": "Стажер",
-        "year": 2015,
-        "description": "70-летний вдовец Бен Уитакер обнаруживает, \
-         что выход на пенсию — еще не конец. Пользуясь случаем, он становится\
-           старшим стажером в интернет-магазине модной одежды под руководством Джулс Остин."
-    },
-    {
-        "title": "Dead Poets Society",
-        "title_ru": "Общество мёртвых поэтов",
-        "year": 1989,
-        "description": "Джон Китинг — новый преподаватель английской словесности в \
-        консервативном американском колледже. От чопорной массы учителей его выгодно \
-        отличают легкость общения, эксцентричное поведение и пренебрежение к программе \
-        обучения. Однажды он посвящает своих подопечных в тайну Общества мёртвых поэтов.\
-        С этого момента каждый из учеников старается обрести свой собственный голос в \
-        безликом хоре, взглянуть на окружающий мир, высоко подпрыгнув над серой школьной оградой."
-    },
-    {
-        "title": "A Beautiful Mind",
-        "title_ru": "Игры разума",
-        "year": 2001,
-        "description": "От всемирной известности до греховных глубин — все это познал на своей \
-        шкуре Джон Форбс Нэш-младший. Математический гений, он на заре своей карьеры сделал\
-        титаническую работу в области теории игр, которая перевернула этот раздел математики\
-        и практически принесла ему международную известность. Однако буквально в то же время \
-        заносчивый и пользующийся успехом у женщин Нэш получает удар судьбы, который переворачивает уже его собственную жизнь."
-    },
-    {
-        "title": "The Imitation Game",
-        "title_ru": "Игра в имитацию",
-        "year": 2014,
-        "description": "Английский математик и логик Алан Тьюринг пытается взломать код немецкой шифровальной машины Enigma во время Второй мировой войны."
-    },
-    {
-        "title": "The Silence of the Lambs",
-        "title_ru": "Молчание ягнят",
-        "year": 1991,
-        "description": "Психопат похищает и убивает молодых женщин по всему Среднему Западу. ФБР, уверенное, что все преступления \
-        совершены одним и тем же человеком, поручает агенту Клариссе Старлинг встретиться с заключенным-маньяком Ганнибалом \
-        Лектером, который мог бы помочь составить психологический портрет убийцы. Сам Лектер отбывает наказание за убийства и каннибализм. Он согласен помочь Клариссе лишь в том случае, если она попотчует его больное воображение подробностями своей личной жизни."
-    }
-]
-
 @lab7.route('/lab7/rest-api/films/', methods=['GET'])
 def get_films():
+    conn, cur = db_connect()
+    cur.execute("SELECT * FROM films")
+    films = cur.fetchall()
+    db_close(conn, cur)
     return jsonify(films)
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
 def get_film(id):
-    if id < 0 or id >= len(films):
+    conn, cur = db_connect()
+    if current_app.config.get('DB_TYPE') == 'postgres':
+        cur.execute("SELECT * FROM films WHERE id = %s", (id,))
+    else:  
+        cur.execute("SELECT * FROM films WHERE id = ?", (id,))
+    film = cur.fetchone()
+    db_close(conn, cur)
+    
+    if not film:
         abort(404)
-    return jsonify(films[id])
+    
+    return jsonify(film)
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE'])
 def del_film(id):
-    if id < 0 or id >= len(films):
+    conn, cur = db_connect()
+    cur.execute("DELETE FROM films WHERE id = %s RETURNING *", (id,))
+    deleted_film = cur.fetchone()
+    db_close(conn, cur)
+    
+    if not deleted_film:
         abort(404)
-    del films[id]
+    
     return '', 204
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
 def put_film(id):
-    if id < 0 or id >= len(films):
-        abort(404)
-
-    film = request.get_json() 
-
-    if film.get('description', '') == '':
+    film = request.get_json()
+    
+    if not film.get('description', ''):
         return jsonify({'description': 'Заполните описание'}), 400
     elif len(film['description']) > 2000:
         return jsonify({'description': 'Описание не должно превышать 2000 символов'}), 400
-
+    
     if not film.get('title') and not film.get('title_ru'):
         return jsonify({'title': 'Заполните поля с названиями'}), 400
+    
     if not film.get('title_ru'):
         return jsonify({'title_ru': 'Заполните русское название'}), 400
-    if film.get('title', '') == '':
-        film['title'] = film['title_ru']
 
     if not film.get('year'):
         return jsonify({'year': 'Укажите год выпуска фильма'}), 400
-    elif not str(film['year']).isdigit() or int(film['year']) < 1895 or int(film['year']) > 2100:
+    elif not str(film['year']).isdigit() or int(film['year']) < 1800 or int(film['year']) > 2100:
         return jsonify({'year': 'Введите корректный год (1800-2100)'}), 400
-        
-    films[id] = film
-    return jsonify(films[id])
+
+    conn, cur = db_connect()
+  
+    if current_app.config.get('DB_TYPE') == 'postgres':
+        cur.execute("""
+            UPDATE films
+            SET title = %s, title_ru = %s, year = %s, description = %s
+            WHERE id = %s RETURNING *
+        """, (film['title'], film['title_ru'], film['year'], film['description'], id))
+    else:  
+        cur.execute("""
+            UPDATE films
+            SET title = ?, title_ru = ?, year = ?, description = ?
+            WHERE id = ? RETURNING *
+        """, (film['title'], film['title_ru'], film['year'], film['description'], id))
+ 
+    updated_film = cur.fetchone()
+    db_close(conn, cur)
+    
+    if not updated_film:
+        abort(404)
+    
+    return jsonify(updated_film)
 
 @lab7.route('/lab7/rest-api/films/', methods=['POST'])
 def add_film():
     film = request.get_json()
-    if not film:
-        abort(400)
-  
-    if film.get('description', '') == '':
+    
+    if not film.get('description', ''):
         return jsonify({'description': 'Заполните описание'}), 400
     elif len(film['description']) > 2000:
         return jsonify({'description': 'Описание не должно превышать 2000 символов'}), 400
-
+    
     if not film.get('title') and not film.get('title_ru'):
         return jsonify({'title': 'Заполните поля с названиями'}), 400
+    
     if not film.get('title_ru'):
         return jsonify({'title_ru': 'Заполните русское название'}), 400
-    
-    if film.get('title', '') == '':
-        film['title'] = film['title_ru']
 
     if not film.get('year'):
         return jsonify({'year': 'Укажите год выпуска фильма'}), 400
-    elif not str(film['year']).isdigit() or int(film['year']) < 1895 or int(film['year']) > 2100:
+    elif not str(film['year']).isdigit() or int(film['year']) < 1800 or int(film['year']) > 2100:
         return jsonify({'year': 'Введите корректный год (1800-2100)'}), 400
-  
 
-    films.append(film)
-    return jsonify(film), 201
+    conn, cur = db_connect()
+    cur.execute("""
+        INSERT INTO films (title, title_ru, year, description)
+        VALUES (%s, %s, %s, %s) RETURNING *
+    """, (film['title'], film['title_ru'], film['year'], film['description']))
+    new_film = cur.fetchone()
+    db_close(conn, cur)
+    
+    return jsonify(new_film), 201
