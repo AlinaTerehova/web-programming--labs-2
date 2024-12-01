4f88eee6397a9a7808f6ebca0c77fa679f9b8af6
Алина Терехова <terehovalinas@yandex.ru>
2024-12-01 13:21:38 +0700

    правки2

--- lab7.py
+++ lab7.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request, jsonify, abort, current_app
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import sqlite3
+from os import path
 
 lab7 = Blueprint('lab7', __name__)
 
